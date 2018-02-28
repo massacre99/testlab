db6a09904c3980418aaa9761198c7c9a3098c25c
massacre99 <klimov.d91@gmail.com>
2018-02-28 13:00:26 +0200

    test

--- src/main/java/com/ms99/app/Main.java
+++ src/main/java/com/ms99/app/Main.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.SkipException;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
         WebDriver driver = initChromeDriver();
+//        WebDriver driver = getDriver();
 
         driver.get("https://www.bing.com/");
         WebElement fieldSearch = driver.findElement(By.id("sb_form_q"));
@@ -29,8 +31,22 @@ public class Main {
         driver.quit();
     }
 
+    // INIT 1
     public static WebDriver initChromeDriver() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        return new ChromeDriver(options);
+    }
+
+    // INIT 2
+    public static WebDriver getDriver() {
+        // get driver path property
+        String driverPath = System.getProperty("user.dir") + "/drivers/chromedriver.exe";
+        if (driverPath == null) {
+            throw new SkipException("Path to ChromeDriver is not specified");
+        }
+        System.setProperty("webdriver.chrome.driver", driverPath);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         return new ChromeDriver(options);
